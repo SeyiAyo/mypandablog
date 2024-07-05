e0de1814b250e2f60c28771b9433e284fb526c1d
SeyiAyo <110205697+SeyiAyo@users.noreply.github.com>
2024-07-05 19:42:24 +0100

    db migration

--- mypandablog/settings.py
+++ mypandablog/settings.py
@@ -23,11 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dutyiwb0-kx^dli$a1iw$bql%s0t1*@swpyw^zu8+f6)1uaicn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
-SECURE_SSL_REDIRECT = True
+# SECURE_SSL_REDIRECT = True
 
 
 # Application definition
@@ -84,8 +84,11 @@ WSGI_APPLICATION = 'mypandablog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'pandablog',
+        'PASSWORD': 'Seyisensei18',
+        'HOST': 'mysql-cheap-honeydew.default.svc.cluster.local',
+        'PORT': '3306',
     }
 }
 
