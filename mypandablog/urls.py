5688dd27b7468ca07095a4695f8883227f776017
SeyiAyo <110205697+SeyiAyo@users.noreply.github.com>
2024-07-10 11:43:49 +0100

    googleanalytics tags

--- mypandablog/urls.py
+++ mypandablog/urls.py
@@ -35,4 +35,6 @@ urlpatterns = [
     path('category/<slug:slug>/', category_detail, name='category_detail'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+
+
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
